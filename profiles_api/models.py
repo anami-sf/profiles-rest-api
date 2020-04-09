5db67ddec441ff10da87b0ca45eb164166091ab5
anami823 <anami823@gmail.com>
2020-04-09 16:27:48 -0700

    Create custom user profile model :

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Create a user profile manager to work with the django build-in admin 
+
+    # UserProfileManager is devined ????
+    objects = UserProfileManager()
+
+    # Override the username filed used to authenticate users
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
+
+
+    
