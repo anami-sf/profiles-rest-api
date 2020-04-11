f53d2175b2ef139cd8915b2761523d58814d054f
anami823 <anami823@gmail.com>
2020-04-10 21:33:25 -0700

    Fix urlpatterns bug

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from profiles_api import views
 
 
-ulrpatterns = [
-    path('hello-view', views.HelloApiView.as_view()),
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
 
