7762b3684e3a24c22a407238dac9565d84ed9b19
anami823 <anami823@gmail.com>
2020-04-10 20:28:34 -0700

    Configure api views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.view import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Itme 1',
+            'Itme 2',
+            'Itme 3',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
