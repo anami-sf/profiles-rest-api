70dbfd9875845760ad7455e198e36838aac56d5f
anami823 <anami823@gmail.com>
2020-04-28 20:53:39 -0700

    Update db to postgresql

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Profiles REST API
 
 Profiles REST API course code.
+
+To Tun the app:
+
+1. Run Vagrant  
+    $ vagrant ssh  
+2. 
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'partner',
+        'USER': 'postgres',
+        'PASSWORD': 'silver1331',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
