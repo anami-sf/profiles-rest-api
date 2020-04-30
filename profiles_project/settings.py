f33065f7cc34fdc8c65a134d759e7d88152c9201
anami <admin@fit-hub.app>
2020-04-30 03:09:06 +0000

    Add STATIC_ROOT to settings

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -125,5 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_URL = STATIC_ROOT
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files_hub")
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
